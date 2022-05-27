36de482594e60dca8bca2fba2f9e39bb7e2a14e3
Wateranlt <Wateranlt@alexis.firhold@yahoo.com>
2022-05-27 18:37:48 +0200

    Forgot some changes

--- SendingFunctions.py
+++ SendingFunctions.py
@@ -4,12 +4,14 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def writeMessage(server, address):
-    name = input("From whom this email is ? : ")
-    adressee = input("Who is the adressee : ")
+    adressee = input("Who is the adressee ? : ")
+    subject = input("What is the subject of the mail ? :")
+    print("Your message : ")
     text = iter(input, '') # Get lines as input until an empty line.
     msgText = '\n'.join(text)
-    msg = message(address, adressee, subject="Mail Test", text=msgText)
+    msg = message(address, adressee, subject=subject, text=msgText)
     server.sendmail(address, adressee, msg=msg.as_string())
+    print("Email Sent !")
     return
 
 def message(address,addressee, subject="Without Subject", 
