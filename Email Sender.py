f623bbab05b7273ad1980a377e3215deca18b267
Wateranlt <Wateranlt@alexis.firhold@yahoo.com>
2022-05-27 18:16:12 +0200

    Started to write the main function: -we can now write multiple mails -layed the fundations of a complete email app (multiple choices) -organized functions in different files

--- Email Sender.py
+++ Email Sender.py
@@ -2,34 +2,37 @@
 
 import smtplib
 import ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import SendingFunctions
 
-inputServer = input("What is the server to use : ")
-adress = input("What is the account to use : ")
-password = input("What is the account's password : ")
-adressee = input("Who is the adressee : ")
-msgText = input("What is the message : ")
+def main():
+    stay = True
+    answer = 1
+    print("=========== Welcome in this email manager ===========")     
+    while(stay):
+        inputServer = input("Which server to want to connect to : ")
+        address = input("What account do you want to use: ")
+        password = input("What is the account's password : ")
+        context = ssl.create_default_context()
+        server = smtplib.SMTP_SSL(inputServer, context=context)
+        server.login(address, password)
+        while(1):
+            print("\n============== What do you want to do ================")
+            print("1. Write a mail")
+            print("2. Read inbox")
+            print("3. Logout of the account")
+            print("4. Quit app")
+            answer = int(input("Your choice ? : "))
+            if(answer == 1):
+                SendingFunctions.writeMessage(server, address)
+            elif(answer == 2):
+                readMessage()
+            elif(answer == 3):
+                break
+            elif(answer == 4):
+                stay = False
+                break
+        server.quit()
 
-def message(subject="Notification", 
-            text=""):
-    
-    # build message contents
-    msg = MIMEMultipart()
-      
-    # Add Subject
-    msg["From"] = adress
-    msg["To"] = adressee
-    msg['Subject'] = subject  
-      
-    # Add text contents
-    msg.attach(MIMEText(text))
-    return msg
 
-context = ssl.create_default_context()
-server = smtplib.SMTP_SSL(inputServer, context=context)
-server.login(adress, password)
-
-msg = message(text=msgText)
-server.sendmail(adress, adressee, msg=msg.as_string())
-server.quit()
+if __name__ == "__main__":
+    main()
